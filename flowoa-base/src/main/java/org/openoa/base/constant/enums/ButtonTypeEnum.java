f08308d89e821b83783ae16cc7166adc8b5965be
caathead <155051876@qq.com>
2025-11-22 16:28:17 +0800

    常见元数据提交:按钮数据,流程节点数据

---
+++ flowoa-base/src/main/java/org/openoa/base/constant/enums/ButtonTypeEnum.java
@@ -0,0 +1,51 @@
+package org.openoa.base.constant.enums;
+
+import lombok.Getter;
+
+/**
+ * @Classname ButtonTypeEnum
+ * @since 0.5
+ * @Created by AntOffice
+ */
+public enum ButtonTypeEnum implements AfEnumBase{
+    BUTTON_TYPE_PREVIEW(0, "预览"),
+    BUTTON_TYPE_SUBMIT(1, "提交"),
+    BUTTON_TYPE_RESUBMIT(2, "重新提交"),
+    BUTTON_TYPE_AGREE(3, "同意"),
+    BUTTON_TYPE_DISAGREE(4, "不同意"),
+    BUTTON_TYPE_BACK_TO_PREV_MODIFY(6, "退回上节点修改"),
+    BUTTON_TYPE_ABANDONED(7, "作废"),
+    BUTTON_TYPE_PRINT(8, "打印"),
+    BUTTON_TYPE_UNDERTAKE(10, "承办"),
+    BUTTON_TYPE_CHANGE_ASSIGNEE(11, "变更处理人"),
+    BUTTON_TYPE_STOP(12, "终止"),
+    BUTTON_TYPE_ADD_ASSIGNEE(13, "添加审批人"),
+    BUTTON_TYPE_FORWARD(15, "抄送"),
+    BUTTON_TYPE_BACK_TO_MODIFY(18, "退回修改"),
+    BUTTON_TYPE_JP(19,"加批"),
+    BUTTON_TYPE_ZB(21,"转办"),
+    BUTTON_TYPE_CHOOSE_ASSIGNEE(22,"自选审批人"),
+    BUTTON_TYPE_BACK_TO_ANY_NODE(23,"退回任意节点"),
+    BUTTON_TYPE_PROCESS_DRAW_BACK(29,"流程撤回")
+    ;
+
+    @Getter
+    private Integer code;
+
+    @Getter
+    private String desc;
+
+    ButtonTypeEnum(Integer code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public static String getDescByCode(Integer code) {
+        for (ButtonTypeEnum buttonTypeEnum : ButtonTypeEnum.values()) {
+            if (buttonTypeEnum.getCode().equals(code)) {
+                return buttonTypeEnum.getDesc();
+            }
+        }
+        return null;
+    }
+}
---
+++ flowoa-base/src/main/java/org/openoa/base/constant/enums/ElementTypeEnum.java
@@ -0,0 +1,27 @@
+package org.openoa.base.constant.enums;
+
+import lombok.Getter;
+
+public enum ElementTypeEnum implements AfEnumBase{
+
+    ELEMENT_TYPE_START_EVENT(1, "StartEvent"),
+    ELEMENT_TYPE_USER_TASK(2, "UserTask"),
+    ELEMENT_TYPE_GATEWAY(3, "Gateway"),
+    ELEMENT_TYPE_SEQUENCE_FLOW(4, "SequenceFlow"),
+    ELEMENT_TYPE_END_EVENT(5, "EndEvent"),
+    ELEMENT_TYPE_PARALLEL_GATEWAY(6,"ParallelGateWay")
+    ;
+
+
+    @Getter
+    private Integer code;
+
+    @Getter
+    private String desc;
+
+
+    ElementTypeEnum(Integer code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+}
