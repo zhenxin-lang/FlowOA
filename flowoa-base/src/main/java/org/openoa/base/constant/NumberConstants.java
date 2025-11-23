1eaddb43ce1e777532c838a9dd1d31f0034cc075
luowenhui2227406033 <3090350135@xiaomi.com>
2025-11-23 10:01:16 +0800

    全局静态常量

---
+++ flowoa-base/src/main/java/org/openoa/base/constant/NumberConstants.java
@@ -0,0 +1,10 @@
+package org.openoa.base.constant;
+
+public class NumberConstants {
+    /**
+     * bpmn name max length
+     */
+    public static final int BPMN_NAME_MAX_LEN = 50;
+
+    public static final int BPMN_FLOW_TYPE_OUTSIDE = 2;
+}
