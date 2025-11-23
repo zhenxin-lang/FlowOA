432bc069c379cc82681aabe9338c6b34806c606e
caathead <155051876@qq.com>
2025-11-23 10:33:27 +0800

    常见工具类提交:bpm工具

---
+++ flowoa-base/src/main/java/org/openoa/base/util/DetailedUserUtil.java
@@ -0,0 +1,29 @@
+package org.openoa.base.util;
+
+import org.openoa.base.entity.DetailedUser;
+import org.openoa.base.vo.BaseIdTranStruVo;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class DetailedUserUtil {
+    public static List<DetailedUser> basicEmployeeInfos(List<BaseIdTranStruVo> litemployeeList){
+        if(CollectionUtils.isEmpty(litemployeeList)){
+            return new ArrayList<>();
+        }
+       return litemployeeList
+               .stream()
+               .map(DetailedUserUtil::basicEmployeeInfo)
+               .collect(Collectors.toList());
+    }
+
+    public static DetailedUser basicEmployeeInfo(BaseIdTranStruVo liteUserInfo){
+      return   DetailedUser.builder()
+                .id(liteUserInfo.getId())
+                .username(liteUserInfo.getName())
+                .build();
+    }
+}
