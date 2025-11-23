4d295e3faa72a097d7543804d34992aeb32fcfb5
caathead <155051876@qq.com>
2025-11-23 10:32:15 +0800

    常见工具类提交:部分工具

---
+++ flowoa-base/src/main/java/org/openoa/base/util/AssertUtil.java
@@ -0,0 +1,30 @@
+package org.openoa.base.util;
+
+import com.google.common.base.Strings;
+import org.openoa.base.exception.AFBizException;
+import org.springframework.util.ObjectUtils;
+
+import java.util.List;
+
+
+public class AssertUtil {
+    public static void throwsIfEmpty(Object object, String errorMsg, List params){
+
+        if(Strings.isNullOrEmpty(errorMsg)){
+            errorMsg="对象不存在";
+        }
+
+
+        StringBuilder sb=new StringBuilder(errorMsg);
+        for (int i = 0; i < params.size(); i++) {
+            sb.append(" %s;");
+        }
+
+        errorMsg=Strings.lenientFormat(sb.toString(),params);
+
+        if(ObjectUtils.isEmpty(object)){
+            throw new AFBizException(errorMsg);
+        }
+
+    }
+}
