0c44528914e810c9b4101b56070e462be79d5187
luowenhui2227406033 <3090350135@xiaomi.com>
2025-11-24 17:57:24 +0800

    feat: 新增基础架构数据传输对象与 Activiti 消息 VO (抽象分页/BPM消息/基础ID结构/基础信息结构)

---
+++ flowoa-base/src/main/java/org/openoa/base/vo/BaseIdTranStruVo.java
@@ -0,0 +1,25 @@
+package org.openoa.base.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseIdTranStruVo implements Serializable {
+    /**
+     * id
+     */
+    private String id;
+
+    /**
+     * name
+     */
+    private String name;
+}
