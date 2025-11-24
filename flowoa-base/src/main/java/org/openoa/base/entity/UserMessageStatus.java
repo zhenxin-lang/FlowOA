672b3fc732f826b91abf15a3b62871d719a13aa5
Nyaan <suyangxiao04@gmail.com>
2025-11-24 17:54:25 +0800

    feat: 新增用户消息及消息状态设置实体 UserMessage.java UserMessageStatus.java

---
+++ flowoa-base/src/main/java/org/openoa/base/entity/UserMessageStatus.java
@@ -0,0 +1,82 @@
+package org.openoa.base.entity;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.openoa.base.interf.TenantField;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("t_user_message_status")
+public class UserMessageStatus implements TenantField, Serializable {
+
+
+    private Integer id;
+    @TableField("user_id")
+    private String userId;
+    /**
+     * message status
+     */
+    @TableField("message_status")
+    private Boolean messageStatus;
+    /**
+     * email status
+     */
+    @TableField("mail_status")
+    private Boolean mailStatus;
+
+    /**
+     * app vibration status
+     */
+    private Boolean shock;
+
+    /**
+     * voice
+     */
+    private Boolean sound;
+
+    /**
+     * is phone number public
+     */
+    @TableField("open_phone")
+    private Boolean openPhone;
+
+    /**
+     * is dnd enabled
+     */
+    @TableField("not_trouble")
+    private Boolean notTrouble;
+
+    /**
+     * dnd start time
+     */
+    @TableField("not_trouble_time_begin")
+    private Date notTroubleTimeBegin;
+
+    /**
+     * dnd end time
+     */
+    @TableField("not_trouble_time_end")
+    private Date notTroubleTimeEnd;
+
+
+    @TableField("create_time")
+    private Date createTime;
+    @TableField("update_time")
+    private Date updateTime;
+    @TableField("create_user")
+    private String createUser;
+    @TableField("update_user")
+    private String updateUser;
+    private Integer isDel;
+    @TableField("tenant_id")
+    private String tenantId;
+}
