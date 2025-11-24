93a4b3f8aac9daf0d2d0a892a9615960f6b899e2
caathead <155051876@qq.com>
2025-11-24 14:09:16 +0800

    实体对象提交:bpm流程节点

---
+++ flowoa-base/src/main/java/org/openoa/base/entity/BpmProcessNameRelevancy.java
@@ -0,0 +1,45 @@
+package org.openoa.base.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.openoa.base.interf.TenantField;
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("bpm_process_name_relevancy")
+public class BpmProcessNameRelevancy implements TenantField, Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+    /**
+     * process name id
+     */
+    @TableField("process_name_id")
+    private Long processNameId;
+    /**
+     * process number
+     */
+    @TableField("process_key")
+    private String processKey;
+    @TableField("is_del")
+    private Integer isDel;
+    @TableField("tenant_id")
+    private String tenantId;
+    @TableField("create_time")
+    private Date createTime;
+
+}
