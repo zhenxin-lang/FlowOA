d7a68466609f9f0eea0b1fc6527c2052f57d0c5e
Nyaan <suyangxiao04@gmail.com>
2025-11-25 09:07:41 +0800

    feat: 新增流程 DIY 配置、委托、枚举、字段属性及表单配置封装 VO DIYProcessInfoDTO.java Entrust.java EnumerateVo.java FieldAttributeInfoVO.java FormConfigWrapper.java

---
+++ flowoa-base/src/main/java/org/openoa/base/vo/Entrust.java
@@ -0,0 +1,43 @@
+package org.openoa.base.vo;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+public class Entrust implements Serializable {
+
+    /**
+     * start time
+     */
+    private Date beginTime;
+
+    private String powerId;
+    /**
+     * end time
+     */
+    private Date endTime;
+    private Integer sender;
+    /**
+     * entrust name
+     */
+    private String name;
+
+    private Integer id;
+
+    /**
+     * receiver id
+     */
+    private String receiverId;
+
+    /**
+     * receiver name
+     */
+    private String receiverName;
+    /**
+     */
+    private Date createTime;
+
+}
