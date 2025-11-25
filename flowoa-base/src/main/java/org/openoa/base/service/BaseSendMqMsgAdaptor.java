dc9b4ee36754bba002ff2a583b15244baf97a58f
lr <2104183633@qq.com>
2025-11-26 00:06:16 +0800

    AntFlowOrderPostProcessor.java AntFlowOrderPreProcessor.java BaseSendMqMsgAdaptor.java BpmVariableService.java BpmVariableSignUpPersonnelService.java

---
+++ flowoa-base/src/main/java/org/openoa/base/service/BaseSendMqMsgAdaptor.java
@@ -0,0 +1,36 @@
+package org.openoa.base.service;
+
+
+import com.alibaba.fastjson2.JSON;
+import org.apache.commons.lang3.NotImplementedException;
+import org.openoa.base.interf.BusinessCallBackAdaptor;
+import org.openoa.base.util.SpringBeanUtils;
+
+
+public abstract class BaseSendMqMsgAdaptor<R, P> implements BusinessCallBackAdaptor<R, P> {
+
+    @Override
+    public void doCallBack(P param) {
+        R r = formattedValue(param);
+        if (r==null) {
+            return;
+        }
+        sendMqMessage(r);
+    }
+
+    private void sendMqMessage(R value) {
+        if (value==null) {
+            return;
+        }
+        String message = JSON.toJSONString(value);
+        //todo
+        throw new NotImplementedException("send mq message method not implemented yet at the moment");
+    }
+
+    protected abstract String getTopicName();
+
+    protected String getUserName() {
+        //todo
+        throw new NotImplementedException("not implemented yet");
+    }
+}
---
+++ flowoa-base/src/main/java/org/openoa/base/service/BpmVariableService.java
@@ -0,0 +1,22 @@
+package org.openoa.base.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.openoa.base.entity.BpmVariable;
+import org.openoa.base.vo.BaseIdTranStruVo;
+
+import java.util.List;
+import java.util.Map;
+
+public interface BpmVariableService extends IService<BpmVariable> {
+    Map<String,String> getAssigneeNameByProcessNumAndElementId(String processNumber, String elementId);
+    String getVarNameByProcessNumberAndElementId(String processNum, String elementId);
+
+    void addNodeAssignees(String processNumber, String elementId, List<BaseIdTranStruVo> assignees);
+
+    void  updateAssignee(String processNumber, String elementId, String assignee, BaseIdTranStruVo newAssigneeInfo);
+
+    void updateAssigneeById(String id, String processNumber,String elementId,String assignee,BaseIdTranStruVo newAssigneeInfo);
+
+    List<String> getElementIdsdByNodeId(String processNumber, String nodeId);
+    List<String> getVarNamesdByNodeId(String processNumber, String nodeId);
+}
