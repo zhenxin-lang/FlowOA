958d0834e423875c76365429ee704621ebf597ce
caathead <155051876@qq.com>
2025-11-25 16:37:07 +0800

    view object对象提交:按钮节点

---
+++ flowoa-base/src/main/java/org/openoa/base/vo/PrevEmployeeInfo.java
@@ -0,0 +1,30 @@
+package org.openoa.base.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PrevEmployeeInfo implements Serializable {
+
+    private static final long serialVersionUID = 4309495676966582583L;
+
+    private Integer employeeId;
+
+    private String employeeName;
+
+    private Long jobLevelId;
+
+    private String jobLevel;
+
+    private String departName;
+
+    private Integer departId;
+}
