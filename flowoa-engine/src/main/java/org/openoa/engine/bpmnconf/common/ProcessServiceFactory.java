d4b1bef44677e822f57feecf17658da05975a684
Nyaan <suyangxiao04@gmail.com>
2025-11-27 17:16:28 +0800

    feat: 新增流程服务工厂与任务管理 Service 实现 ProcessServiceFactory.java TaskMgmtServiceImpl.java

---
+++ flowoa-engine/src/main/java/org/openoa/engine/bpmnconf/common/ProcessServiceFactory.java
@@ -0,0 +1,69 @@
+package org.openoa.engine.bpmnconf.common;
+
+import org.activiti.engine.*;
+import org.openoa.engine.bpmnconf.mapper.BpmBusinessProcessMapper;
+import org.openoa.engine.bpmnconf.mapper.BpmTaskconfigMapper;
+import org.openoa.engine.bpmnconf.mapper.TaskMgmtMapper;
+import org.openoa.engine.bpmnconf.service.biz.BpmBusinessProcessServiceImpl;
+import org.openoa.engine.bpmnconf.service.impl.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * process service factory
+ * @author ：AntFlow
+ * @since 0.5
+ */
+@Component
+public class ProcessServiceFactory {
+    @Autowired
+    protected RepositoryService repositoryService;
+
+    @Autowired
+    protected RuntimeService runtimeService;
+
+    @Autowired
+    protected IdentityService identityService;
+
+    @Autowired
+    protected TaskService taskService;
+
+
+    @Autowired
+    protected ManagementService managementService;
+
+    @Autowired
+    protected BpmFlowruninfoServiceImpl bpmFlowruninfoMapper;
+
+
+    @Autowired
+    protected BpmTaskconfigMapper bpmTaskconfigMapper;
+
+
+    @Autowired
+    protected BpmBusinessProcessMapper bpmBusinessProcessMapper;
+
+    @Autowired
+    protected BpmTaskconfigServiceImpl bpmTaskconfigService;
+
+
+    @Autowired
+    protected BpmFlowrunEntrustServiceImpl entrustService;
+
+    @Autowired
+    protected BpmBusinessProcessServiceImpl bpmBusinessProcessService;
+
+
+    @Autowired
+    protected TaskMgmtMapper taskMgmtMapper;
+
+    @Autowired
+    protected ProcessEngine processEngine;
+
+
+    @Autowired
+    protected TaskMgmtServiceImpl taskMgmtService;
+    @Autowired
+    protected UserMessageServiceImpl userMessageService;
+}
