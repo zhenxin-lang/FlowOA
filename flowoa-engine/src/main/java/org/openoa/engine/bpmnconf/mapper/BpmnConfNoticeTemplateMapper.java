a72814990bdc8e86ac21f7a5efb6c1cb865d272e
Nyaan <suyangxiao04@gmail.com>
2025-11-27 21:48:43 +0800

    feat: 新增流程配置、通知模板及节点业务/审批层级相关 Mapper BpmnConfMapper.java BpmnConfNoticeTemplateDetailMapper.java BpmnConfNoticeTemplateMapper.java BpmnNodeAssignLevelConfMapper.java BpmnNodeBusinessTableConfMapper.java

---
+++ flowoa-engine/src/main/java/org/openoa/engine/bpmnconf/mapper/BpmnConfNoticeTemplateMapper.java
@@ -0,0 +1,15 @@
+package org.openoa.engine.bpmnconf.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.openoa.base.entity.BpmnConfNoticeTemplate;
+
+/**
+ * @Classname BpmnConfNoticeTemplateMapper
+ * @Date 2021-11-06 8:01
+ * @Created by AntOffice
+ */
+@Mapper
+public interface BpmnConfNoticeTemplateMapper extends BaseMapper<BpmnConfNoticeTemplate> {
+
+}
