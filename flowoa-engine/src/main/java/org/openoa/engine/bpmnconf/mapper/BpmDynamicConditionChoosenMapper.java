d185bfc9aab2f530abead9e2c4595ab7410d6d2d
MorisWu <3081045655@qq.com>
2025-11-27 21:24:00 +0800

    src/main/java/org/openoa/engine/bpmnconf/mapper/BpmFlowrunEntrustMapper.java

---
+++ flowoa-engine/src/main/java/org/openoa/engine/bpmnconf/mapper/BpmDynamicConditionChoosenMapper.java
@@ -0,0 +1,9 @@
+package org.openoa.engine.bpmnconf.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.openoa.base.entity.BpmDynamicConditionChoosen;
+
+@Mapper
+public interface BpmDynamicConditionChoosenMapper extends BaseMapper<BpmDynamicConditionChoosen> {
+}
