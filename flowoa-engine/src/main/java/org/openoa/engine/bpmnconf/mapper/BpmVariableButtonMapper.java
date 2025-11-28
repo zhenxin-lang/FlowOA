2e61339fad8e96ed0ba44aab202f04f42edc64ab
lr <2104183633@qq.com>
2025-11-29 01:55:34 +0800

    BpmVariableButtonMapper.java BpmVariableMapper.java BpmVariableMessageMapper.java BpmVariableSequenceFlowMapper.java BpmVariableSignUpMapper.java

---
+++ flowoa-engine/src/main/java/org/openoa/engine/bpmnconf/mapper/BpmVariableButtonMapper.java
@@ -0,0 +1,22 @@
+package org.openoa.engine.bpmnconf.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.openoa.base.entity.BpmVariableButton;
+
+import java.util.List;
+
+
+@Mapper
+public interface BpmVariableButtonMapper extends BaseMapper<BpmVariableButton> {
+    /**
+     * query node button by process number and element id
+     *
+     * @param processNum
+     * @param elementId
+     * @return
+     */
+    List<BpmVariableButton> getButtonsByProcessNumber(@Param("processNum") String processNum,
+                                                      @Param("elementId") String elementId);
+}
