79e4abaf0014df96d9e58aed627e6b539c24e271
MorisWu <3081045655@qq.com>
2025-11-29 00:15:49 +0800

    src/main/java/org/openoa/engine/bpmnconf/mapper/BpmVariableApproveRemindMapper.java

---
+++ flowoa-engine/src/main/java/org/openoa/engine/bpmnconf/mapper/BpmProcessNodeSubmitMapper.java
@@ -0,0 +1,10 @@
+package org.openoa.engine.bpmnconf.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.openoa.base.entity.BpmProcessNodeSubmit;
+
+@Mapper
+public interface BpmProcessNodeSubmitMapper extends BaseMapper<BpmProcessNodeSubmit> {
+
+}
