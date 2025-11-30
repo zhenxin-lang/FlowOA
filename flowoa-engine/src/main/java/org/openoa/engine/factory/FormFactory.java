1ce1c9012d55a00f4bf3225862c447d850b138cd
Nyaan <suyangxiao04@gmail.com>
2025-11-30 18:17:30 +0800

    feat: 新增第三方回调工厂，支持签名、业务方标识及统一 POST 回调机制 FormFactory.java IAdaptorFactory.java SimpleProxyFactory.java SpfService.java TagParser.java ThirdPartyCallbackFactory.java

---
+++ flowoa-engine/src/main/java/org/openoa/engine/factory/FormFactory.java
@@ -0,0 +1,101 @@
+package org.openoa.engine.factory;
+
+import com.alibaba.fastjson2.JSON;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import org.openoa.base.constant.StringConstants;
+import org.openoa.base.exception.AFBizException;
+import org.openoa.base.interf.ActivitiService;
+import org.openoa.base.interf.FormOperationAdaptor;
+import org.openoa.base.vo.BusinessDataVo;
+import org.openoa.base.entity.OutSideBpmAccessBusiness;
+import org.openoa.engine.bpmnconf.service.impl.OutSideBpmAccessBusinessServiceImpl;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.ObjectUtils;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.List;
+
+@Component
+public class FormFactory implements ApplicationContextAware {
+    @Autowired
+    private IAdaptorFactory adaptorFactory;
+    @Autowired
+    @Lazy
+    private OutSideBpmAccessBusinessServiceImpl outSideBpmAccessBusinessService;
+    private ApplicationContext applicationContext;
+
+    public FormOperationAdaptor getFormAdaptor(String formCode){
+        return getFormAdaptor(BusinessDataVo.builder().formCode(formCode).build());
+    }
+    public FormOperationAdaptor getFormAdaptor(BusinessDataVo vo) {
+        if (ObjectUtils.isEmpty(vo)) {
+            return null;
+        }
+        ActivitiService activitiService = adaptorFactory.getActivitiService(vo);
+        if (ObjectUtils.isEmpty(activitiService)) {
+            throw new AFBizException("form code does not have a processing bean！");
+        }
+        return (FormOperationAdaptor) activitiService;
+    }
+
+    /**
+     *
+     * @param params the request body string
+     * @param formCode if caller can't provide,pass null
+     * @return
+     */
+    public BusinessDataVo dataFormConversion(String params,String formCode) {
+        BusinessDataVo vo = JSON.parseObject(params, BusinessDataVo.class);
+        if(formCode==null){
+            formCode=vo.getFormCode();
+        }
+        if(vo.getIsOutSideAccessProc()){
+            LambdaQueryWrapper<OutSideBpmAccessBusiness> qryWrapper = Wrappers
+                    .<OutSideBpmAccessBusiness>lambdaQuery()
+                    .eq(OutSideBpmAccessBusiness::getProcessNumber, vo.getProcessNumber());
+            List<OutSideBpmAccessBusiness> bpmAccessBusinesses = outSideBpmAccessBusinessService.list(qryWrapper);
+            if(!CollectionUtils.isEmpty(bpmAccessBusinesses)){
+                vo.setFormData(bpmAccessBusinesses.get(0).getFormDataPc());
+            }
+
+        }
+        if(vo.getIsLowCodeFlow()!=null&&vo.getIsLowCodeFlow()==1){
+            formCode=StringConstants.LOWFLOW_FORM_CODE;
+        }
+        Object bean = applicationContext.getBean(formCode);
+        if (ObjectUtils.isEmpty(bean)) {
+            throw new AFBizException("can not get the processing bean by form code:{}!"+formCode);
+        }
+        return JSON.parseObject(params, (Type) getFormTClass(formCode));
+
+    }
+
+    private Class<?> getFormTClass(String key) {
+        FormOperationAdaptor bean = getFormAdaptor(BusinessDataVo.builder().formCode(key).build());
+        if (!ObjectUtils.isEmpty(bean)) {
+
+            Type[] interfacesTypes = ClassUtils.getUserClass(bean).getGenericInterfaces();
+            ParameterizedType p = (ParameterizedType) interfacesTypes[0];
+            Class<?> cls = (Class) p.getActualTypeArguments()[0];
+            if (!ObjectUtils.isEmpty(cls)) {
+                return cls;
+            }
+        }
+        throw new AFBizException("该表单未关联业务实现类或未关联实现类泛型！");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+}
---
+++ flowoa-engine/src/main/java/org/openoa/engine/factory/IAdaptorFactory.java
@@ -0,0 +1,45 @@
+package org.openoa.engine.factory;
+
+
+import org.openoa.base.constant.enums.MessageSendTypeEnum;
+import org.openoa.base.constant.enums.NodePropertyEnum;
+import org.openoa.base.constant.enums.OrderNodeTypeEnum;
+import org.openoa.base.interf.FormOperationAdaptor;
+import org.openoa.base.interf.ProcessNoticeAdaptor;
+import org.openoa.base.interf.ProcessOperationAdaptor;
+import org.openoa.base.interf.anno.AutoParse;
+import org.openoa.base.vo.BusinessDataVo;
+import org.openoa.common.adaptor.AbstractBpmnPersonnelAdaptor;
+import org.openoa.common.adaptor.bpmnelementadp.AbstractOrderedSignNodeAdp;
+import org.openoa.common.adaptor.bpmnelementadp.BpmnElementAdaptor;
+import org.openoa.engine.bpmnconf.adp.bpmnnodeadp.BpmnNodeAdaptor;
+import org.openoa.engine.bpmnconf.adp.personneladp.AbstractBusinessConfigurationAdaptor;
+import org.openoa.engine.bpmnconf.constant.enus.BpmnNodeAdpConfEnum;
+import org.openoa.engine.bpmnconf.constant.enus.ConfigurationTableAdapterEnum;
+import org.openoa.engine.bpmnconf.service.tagparser.*;
+
+public interface IAdaptorFactory {
+    @SpfService(tagParser = ActivitiTagParser.class)
+    public FormOperationAdaptor getActivitiService(BusinessDataVo dataVo);
+
+    @SpfService(tagParser = PersonnelTagParser.class)
+    AbstractBpmnPersonnelAdaptor getPersonnelAdaptor(NodePropertyEnum nodePropertyEnum);
+
+    @SpfService(tagParser= OrderedSignTagParser.class)
+    AbstractOrderedSignNodeAdp getOrderedSignNodeAdp(OrderNodeTypeEnum orderNodeTypeEnum);
+
+    @SpfService(tagParser = FormOperationTagParser.class)
+    ProcessOperationAdaptor getProcessOperation(BusinessDataVo vo);
+
+    @SpfService(tagParser = BpmnNodeAdaptorTagParser.class)
+    BpmnNodeAdaptor getBpmnNodeAdaptor(BpmnNodeAdpConfEnum adpConfEnum);
+
+    //@SpfService(tagParser = BpmnElementAdaptorTagParser.class)
+    @AutoParse
+    BpmnElementAdaptor getBpmnElementAdaptor(NodePropertyEnum nodePropertyEnum);
+
+    @AutoParse
+    AbstractBusinessConfigurationAdaptor getBusinessConfigurationAdaptor(ConfigurationTableAdapterEnum tableAdapterEnum);
+    @AutoParse
+    ProcessNoticeAdaptor getProcessNoticeAdaptor(MessageSendTypeEnum messageSendTypeEnum);
+}
