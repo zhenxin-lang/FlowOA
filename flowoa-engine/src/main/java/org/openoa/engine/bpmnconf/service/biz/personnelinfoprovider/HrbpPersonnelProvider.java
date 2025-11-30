a0d5f3db321f36eeb7e469237f2be37087998ff1
luowenhui2227406033 <3090350135@xiaomi.com>
2025-11-30 19:53:50 +0800

    feat: 扩展 BPMN 节点指派人提供者实现 (表单相关人员/HRBP/职级/循环)

---
+++ flowoa-engine/src/main/java/org/openoa/engine/bpmnconf/service/biz/personnelinfoprovider/HrbpPersonnelProvider.java
@@ -0,0 +1,29 @@
+package org.openoa.engine.bpmnconf.service.biz.personnelinfoprovider;
+
+import com.google.common.collect.Lists;
+import org.openoa.base.service.AfUserService;
+import org.openoa.base.vo.BaseIdTranStruVo;
+import org.openoa.base.vo.BpmnNodeParamsAssigneeVo;
+import org.openoa.base.vo.BpmnNodeVo;
+import org.openoa.base.vo.BpmnStartConditionsVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @Author TylerZhou
+ * @Date 2024/7/19 19:52
+ * @Version 1.0
+ */
+@Component
+public class HrbpPersonnelProvider extends AbstractDifferentStandardAssignNodeAssigneeVoProvider{
+    @Autowired
+    private AfUserService userService;
+
+    @Override
+    protected List<BaseIdTranStruVo> queryUsers(List<String> users) {
+        return userService.queryEmployeeHrpbByEmployeeIds(users);
+    }
+}
