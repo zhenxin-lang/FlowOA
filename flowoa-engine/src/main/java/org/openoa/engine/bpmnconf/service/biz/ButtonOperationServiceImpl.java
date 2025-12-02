2f5e688e73b57116f46a1d7db11f4ba3639bd5a5
luowenhui2227406033 <3090350135@xiaomi.com>
2025-12-02 15:45:59 +0800

    feat: 新增 BPM 变量签约、验证信息及基础字典服务实现 (变量签约/签约人员/验证信息/按钮操作/字典数据)

---
+++ flowoa-engine/src/main/java/org/openoa/engine/bpmnconf/service/biz/ButtonOperationServiceImpl.java
@@ -0,0 +1,33 @@
+package org.openoa.engine.bpmnconf.service.biz;
+
+import lombok.extern.slf4j.Slf4j;
+import org.openoa.base.interf.ProcessOperationAdaptor;
+import org.openoa.base.service.ProcessorFactory;
+import org.openoa.base.vo.BusinessDataVo;
+import org.openoa.engine.factory.IAdaptorFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+public class ButtonOperationServiceImpl{
+    @Autowired
+    private IAdaptorFactory adaptorFactory;
+
+    @Transactional(rollbackFor = Exception.class)
+    public BusinessDataVo buttonsOperationTransactional(BusinessDataVo vo) {
+
+        //Do button operations
+        ProcessOperationAdaptor processOperation = adaptorFactory.getProcessOperation(vo);
+        try {
+            processOperation.doProcessButton(vo);
+            ProcessorFactory.executePostProcessors(vo);
+        } catch (Exception e){
+            log.error("流程执行出错啦!",e);
+            throw e;
+        }
+        return vo;
+
+    }
+}
