54f5efc4f12dc681179c85519a00401b7e6b2f50
luowenhui2227406033 <3090350135@xiaomi.com>
2025-12-03 14:59:05 +0800

    feat: 完善 BPMN 节点条件与模板标记适配器 (采购类型/总金额/模板标记)

---
+++ flowoa-engine/src/main/java/org/openoa/engine/bpmnconf/adp/conditionfilter/nodetypeconditions/BpmnTemplateMarkAdp.java
@@ -0,0 +1,31 @@
+package org.openoa.engine.bpmnconf.adp.conditionfilter.nodetypeconditions;
+
+import lombok.extern.slf4j.Slf4j;
+import org.openoa.base.vo.BpmnNodeConditionsConfBaseVo;
+import org.openoa.base.entity.OutSideBpmConditionsTemplate;
+import org.openoa.engine.bpmnconf.service.impl.OutSideBpmConditionsTemplateServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
+public class BpmnTemplateMarkAdp extends BpmnNodeConditionsAdaptor {
+
+    @Autowired
+    private OutSideBpmConditionsTemplateServiceImpl outSideBpmConditionsTemplateService;
+
+    @Override
+    public void setConditionsResps(BpmnNodeConditionsConfBaseVo bpmnNodeConditionsConfBaseVo) {
+        if (!CollectionUtils.isEmpty(bpmnNodeConditionsConfBaseVo.getTemplateMarks())) {
+            List<OutSideBpmConditionsTemplate> outSideBpmConditionsTemplates = outSideBpmConditionsTemplateService.getBaseMapper().selectBatchIds(bpmnNodeConditionsConfBaseVo.getTemplateMarks());
+            if (!CollectionUtils.isEmpty(outSideBpmConditionsTemplates)) {
+                List<Integer> collect = outSideBpmConditionsTemplates.stream().map(o -> o.getId().intValue()).collect(Collectors.toList());
+                bpmnNodeConditionsConfBaseVo.setTemplateMarks(collect);
+            }
+        }
+    }
+}
