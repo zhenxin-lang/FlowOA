f3e09082e28bae17a71835ac8ec6ffad9ac689a2
caathead <155051876@qq.com>
2025-12-03 23:44:28 +0800

    剩余配置类提交

---
+++ flowoa-engine/src/main/java/org/openoa/engine/conf/config/ProxyFactoryBeanConfig.java
@@ -0,0 +1,16 @@
+package org.openoa.engine.conf.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class ProxyFactoryBeanConfig implements ApplicationContextAware {
+    private ApplicationContext applicationContext;
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext=applicationContext;
+    }
+}
