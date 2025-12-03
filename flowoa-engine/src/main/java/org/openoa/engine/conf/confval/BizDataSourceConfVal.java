86e801220089acd722129d05db64ab07248048ce
caathead <155051876@qq.com>
2025-12-04 00:01:41 +0800

    线程池配置

---
+++ flowoa-engine/src/main/java/org/openoa/engine/conf/confval/BizDataSourceConfVal.java
@@ -0,0 +1,23 @@
+/*
+package org.openoa.engine.conf.confval;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@EqualsAndHashCode(callSuper=false)
+@Configuration
+public class BizDataSourceConfVal extends DataSourceConfVal {
+    @Value("${jimubiz.datasource.url}")
+    private String url;
+
+    @Value("${jimubiz.datasource.username}")
+    private String username;
+
+    @Value("${jimubiz.datasource.password}")
+    private String password;
+
+}
+*/
