0e12668c6e88fd9126253821fca553d09a404c18
Nyaan <suyangxiao04@gmail.com>
2025-12-03 15:02:33 +0800

    feat: 新增流程启动/移除格式化工厂及默认实现，支持节点复制与待移除人员过滤 BpmnRemoveConfFormatFactory.java BpmnRemoveCopyFormatImpl.java BpmnRemoveFormatImpl.java BpmnStartFormat.java BpmnStartFormatFactory.java

---
+++ flowoa-engine/src/main/java/org/openoa/engine/bpmnconf/adp/formatter/BpmnRemoveCopyFormatImpl.java
@@ -0,0 +1,49 @@
+package org.openoa.engine.bpmnconf.adp.formatter;
+
+
+import com.google.common.collect.Lists;
+import org.openoa.base.constant.enums.NodeTypeEnum;
+import org.openoa.base.vo.BpmnConfVo;
+import org.openoa.base.vo.BpmnNodeVo;
+import org.openoa.base.vo.BpmnStartConditionsVo;
+import org.openoa.common.formatter.AbstractBpmnRemoveFormat;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+@Service
+public class BpmnRemoveCopyFormatImpl extends AbstractBpmnRemoveFormat {
+    @Override
+    public int order() {
+        return 2;
+    }
+
+    @Override
+    public void removeBpmnConf(BpmnConfVo bpmnConfVo, BpmnStartConditionsVo bpmnStartConditions) {
+        super.removeBpmnConf(bpmnConfVo,bpmnStartConditions);
+    }
+
+    @Override
+    public List<Supplier<Boolean>> trueSuppliers(BpmnNodeVo vo, BpmnStartConditionsVo bpmnStartConditions) {
+        Supplier<Boolean> supplier = new Supplier<Boolean>() {
+            @Override
+            public Boolean get() {
+               if(bpmnStartConditions.isPreview()){
+                   return false;
+               }else{
+                   return vo.getNodeType().equals(NodeTypeEnum.NODE_TYPE_COPY.getCode());
+               }
+            }
+        };
+        if(supplier.get()){
+            if (bpmnStartConditions.getEmpToForwardList() == null) {
+                bpmnStartConditions.setEmpToForwardList(new ArrayList<>());
+            }
+            vo.setEmpToForwardList(vo.getProperty().getEmplList());
+            bpmnStartConditions.getEmpToForwardList().addAll(vo.getProperty().getEmplIds());
+        }
+        return Lists.newArrayList(supplier);
+    }
+}
