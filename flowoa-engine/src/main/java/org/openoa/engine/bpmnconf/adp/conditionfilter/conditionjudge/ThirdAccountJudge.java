dea8a9b09956aeec51cc9f6ff551dccc6e84dd80
luowenhui2227406033 <3090350135@xiaomi.com>
2025-12-03 14:53:21 +0800

    feat: 完善 BPMN 配置条件判断逻辑 (Spel表达式/第三方账户/总金额)

---
+++ flowoa-engine/src/main/java/org/openoa/engine/bpmnconf/adp/conditionfilter/conditionjudge/ThirdAccountJudge.java
@@ -0,0 +1,25 @@
+package org.openoa.engine.bpmnconf.adp.conditionfilter.conditionjudge;
+
+import lombok.extern.slf4j.Slf4j;
+import org.openoa.engine.bpmnconf.adp.conditionfilter.ConditionJudge;
+import org.openoa.base.vo.BpmnNodeConditionsConfBaseVo;
+import org.openoa.base.vo.BpmnStartConditionsVo;
+import org.openoa.base.exception.AFBizException;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+@Service
+@Slf4j
+public class ThirdAccountJudge implements ConditionJudge {
+
+    @Override
+    public boolean judge(String nodeId, BpmnNodeConditionsConfBaseVo conditionsConf, BpmnStartConditionsVo bpmnStartConditionsVo, int group) {
+        if (ObjectUtils.isEmpty(conditionsConf.getAccountType())) {
+            throw new AFBizException("the process has no third party account conf,please contact the administrator to add one");
+        }
+        if (ObjectUtils.isEmpty(bpmnStartConditionsVo.getAccountType())) {
+            return false;
+        }
+        return conditionsConf.getAccountType().contains(bpmnStartConditionsVo.getAccountType());
+    }
+}
