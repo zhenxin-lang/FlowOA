d40a388952e24ece34c5c4c6c4dab28c1fc3c511
MorisWu <3081045655@qq.com>
2025-12-03 13:13:26 +0800

    src/main/java/org/openoa/engine/bpmnconf/adp/conditionfilter/conditionjudge/LFDateConditionJudge.java

---
+++ flowoa-engine/src/main/java/org/openoa/engine/bpmnconf/adp/conditionfilter/conditionjudge/BpmnTemplateMarkJudge.java
@@ -0,0 +1,24 @@
+package org.openoa.engine.bpmnconf.adp.conditionfilter.conditionjudge;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.openoa.base.vo.BpmnNodeConditionsConfBaseVo;
+import org.openoa.base.vo.BpmnStartConditionsVo;
+import org.openoa.engine.bpmnconf.adp.conditionfilter.ConditionJudge;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+
+@Slf4j
+@Component
+public class BpmnTemplateMarkJudge implements ConditionJudge {
+    @Override
+    public boolean judge(String nodeId, BpmnNodeConditionsConfBaseVo conditionsConf, BpmnStartConditionsVo bpmnStartConditionsVo, int group) {
+        if (!CollectionUtils.isEmpty(conditionsConf.getTemplateMarks()) &&
+                CollectionUtils.containsAny(conditionsConf.getTemplateMarks(),bpmnStartConditionsVo.getTemplateMarkIds())
+        ) {
+            return true;
+        }
+        return false;
+    }
+}
