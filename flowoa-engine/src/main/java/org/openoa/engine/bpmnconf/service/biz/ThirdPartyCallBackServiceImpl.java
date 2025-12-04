90b991b413ed7744eac829dcda1fdcd459fa3b4d
luowenhui2227406033 <3090350135@xiaomi.com>
2025-12-04 14:46:22 +0800

    feat: 新增第三方回调、Activiti 与用户消息业务服务 (第三方回调/传统Activiti/用户消息业务/用户消息预处理)

---
+++ flowoa-engine/src/main/java/org/openoa/engine/bpmnconf/service/biz/ThirdPartyCallBackServiceImpl.java
@@ -0,0 +1,28 @@
+package org.openoa.engine.bpmnconf.service.biz;
+
+import lombok.extern.slf4j.Slf4j;
+import org.openoa.base.constant.enums.CallbackTypeEnum;
+import org.openoa.base.interf.MethodReplay;
+import org.openoa.base.vo.BpmnConfVo;
+import org.openoa.engine.bpmnconf.service.impl.BpmVariableMessageServiceImpl;
+import org.openoa.engine.bpmnconf.service.interf.repository.BpmVariableMessageService;
+import org.openoa.engine.factory.ThirdPartyCallbackFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import static org.openoa.base.constant.enums.CallbackTypeEnum.PROC_END_CALL_BACK;
+@Slf4j
+@Service
+public class ThirdPartyCallBackServiceImpl {
+    @Autowired
+    private BpmVariableMessageService bpmVariableMessageService;
+
+    @MethodReplay
+   public void doCallback(CallbackTypeEnum callbackTypeEnum, BpmnConfVo bpmnConfVo,
+               String processNum, String businessId,String verifyUserName){
+       log.info("准备执行外部工作流回调：{} , processNumber:{} , callBackUrl:{} , 操作人：{}",PROC_END_CALL_BACK.getDesc() ,processNum, verifyUserName);
+       //回调通知业务方
+       ThirdPartyCallbackFactory.build().doCallback( callbackTypeEnum, bpmnConfVo,
+               processNum, businessId);
+   }
+}
