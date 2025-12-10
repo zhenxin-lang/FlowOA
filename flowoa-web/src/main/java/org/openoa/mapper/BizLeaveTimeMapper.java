4752f922db95114b74ce8eb403e6d482fc51d220
Nyaan <suyangxiao04@gmail.com>
2025-12-10 09:20:16 +0800

    feat: 新增请假、采购、退款、车务加油业务 Mapper

---
+++ flowoa-web/src/main/java/org/openoa/mapper/BizLeaveTimeMapper.java
@@ -0,0 +1,9 @@
+package org.openoa.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.openoa.entity.BizLeaveTime;
+
+@Mapper
+public interface BizLeaveTimeMapper extends BaseMapper<BizLeaveTime> {
+}
