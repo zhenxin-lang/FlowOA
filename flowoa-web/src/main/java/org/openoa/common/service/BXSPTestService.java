223723d95752f2aea9d29b931f0e55b668b48914
Nyaan <suyangxiao04@gmail.com>
2025-12-10 19:24:15 +0800

    feat: 新增请假、报销、采购、第三方账号、车务加油等示例业务 Service，演示流程接入与审批实现

---
+++ flowoa-web/src/main/java/org/openoa/common/service/BXSPTestService.java
@@ -0,0 +1,95 @@
+package org.openoa.common.service;
+
+import org.openoa.base.constant.enums.ButtonTypeEnum;
+import org.openoa.base.interf.ActivitiService;
+import org.openoa.base.interf.ActivitiServiceAnno;
+import org.openoa.base.interf.FormOperationAdaptor;
+import org.openoa.base.util.SecurityUtils;
+import org.openoa.base.vo.BpmnStartConditionsVo;
+import org.openoa.base.vo.BusinessDataVo;
+import org.openoa.entity.BizRefund;
+import org.openoa.mapper.BizRefundMapper;
+import org.openoa.vo.BizRefundVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+
+@ActivitiServiceAnno(svcName = "BXSP_WMA",desc = "报销审批测试")
+//formAdaptor
+public class BXSPTestService implements FormOperationAdaptor<BizRefundVo>, ActivitiService {
+
+    @Autowired
+    private BizRefundMapper bizRefundMapper;
+
+    @Override
+    public BpmnStartConditionsVo previewSetCondition(BizRefundVo vo) {
+        String userId =  vo.getStartUserId();
+        return BpmnStartConditionsVo.builder()
+                .startUserId(userId).build();
+    }
+
+    @Override
+    public void initData(BizRefundVo vo) {
+
+    }
+
+    @Override
+    public BpmnStartConditionsVo launchParameters(BizRefundVo vo) {
+        String userId =  vo.getStartUserId();
+        return BpmnStartConditionsVo.builder()
+                .startUserId(userId).build();
+    }
+
+    @Override
+    public void queryData(BizRefundVo vo) {
+        BizRefund refund = bizRefundMapper.selectById(vo.getBusinessId());
+        BeanUtils.copyProperties(refund,vo);
+
+    }
+
+    @Override
+    public void submitData(BizRefundVo vo) {
+        BizRefund entity=new BizRefund();
+        BeanUtils.copyProperties(vo,entity);
+        entity.setCreateTime(new Date());
+        entity.setCreateUser(SecurityUtils.getLogInEmpNameSafe());
+        entity.setRefundUserId(Integer.parseInt(vo.getStartUserId()));
+        entity.setRefundUserName(SecurityUtils.getLogInEmpNameSafe());
+
+        bizRefundMapper.insert(entity);
+        vo.setBusinessId(entity.getId().toString());
+        vo.setProcessTitle("报销申请");
+        vo.setProcessDigest(vo.getRemark());
+        vo.setEntityName(BizRefund.class.getSimpleName());
+
+    }
+
+    @Override
+    public void consentData(BizRefundVo vo) {
+        if (vo.getOperationType().equals(ButtonTypeEnum.BUTTON_TYPE_RESUBMIT.getCode())
+                && !vo.getOperationType().equals(ButtonTypeEnum.BUTTON_TYPE_AGREE.getCode()) ){
+            BizRefund entity = new BizRefund();
+            BeanUtils.copyProperties(vo,entity);
+            Integer id=  Integer.valueOf((vo.getBusinessId()).toString());
+            entity.setId(id);
+            bizRefundMapper.updateById(entity);
+        }
+
+    }
+
+    @Override
+    public void backToModifyData(BizRefundVo vo) {
+
+    }
+
+    @Override
+    public void cancellationData(BizRefundVo vo) {
+
+    }
+
+    @Override
+    public void finishData(BusinessDataVo vo) {
+
+    }
+}
---
+++ flowoa-web/src/main/java/org/openoa/common/service/UcarRefuelTestService.java
@@ -0,0 +1,93 @@
+package org.openoa.common.service;
+
+import org.openoa.base.constant.enums.ButtonTypeEnum;
+import org.openoa.base.interf.ActivitiService;
+import org.openoa.base.interf.ActivitiServiceAnno;
+import org.openoa.base.interf.FormOperationAdaptor;
+import org.openoa.base.util.SecurityUtils;
+import org.openoa.base.vo.BpmnStartConditionsVo;
+import org.openoa.base.vo.BusinessDataVo;
+import org.openoa.entity.BizUcarfuel;
+import org.openoa.mapper.BizUcarFuelMapper;
+import org.openoa.vo.BizUcarRefuelVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+
+@ActivitiServiceAnno(svcName = "UCARREFUEl_WMA",desc = "加油上报")
+//formAdaptor
+public class UcarRefuelTestService implements FormOperationAdaptor<BizUcarRefuelVo>, ActivitiService {
+
+    @Autowired
+    private BizUcarFuelMapper bizUcarfuelMapper;
+
+    @Override
+    public BpmnStartConditionsVo previewSetCondition(BizUcarRefuelVo vo) {
+        String userId =  vo.getStartUserId();
+        return BpmnStartConditionsVo.builder()
+                .startUserId(userId).build();
+    }
+
+    @Override
+    public void initData(BizUcarRefuelVo vo) {
+
+    }
+
+
+    @Override
+    public BpmnStartConditionsVo launchParameters(BizUcarRefuelVo vo) {
+        String userId =  vo.getStartUserId();
+        return BpmnStartConditionsVo.builder()
+                .startUserId(userId).build();
+    }
+
+    @Override
+    public void queryData(BizUcarRefuelVo vo) {
+        BizUcarfuel ucarFuel = bizUcarfuelMapper.selectById(vo.getBusinessId());
+        BeanUtils.copyProperties(ucarFuel,vo);
+
+    }
+
+    @Override
+    public void submitData(BizUcarRefuelVo vo) {
+        BizUcarfuel ucarFuel=new BizUcarfuel();
+        BeanUtils.copyProperties(vo,ucarFuel);
+
+        ucarFuel.setCreateTime(new Date());
+        ucarFuel.setCreateUser(SecurityUtils.getLogInEmpNameSafe());
+
+        bizUcarfuelMapper.insert(ucarFuel);
+        vo.setBusinessId(ucarFuel.getId().toString());
+        vo.setProcessTitle("加油上报");
+        vo.setProcessDigest(vo.getRemark());
+        vo.setEntityName(BizUcarfuel.class.getSimpleName());
+    }
+
+    @Override
+    public void consentData(BizUcarRefuelVo vo) {
+        if (vo.getOperationType().equals(ButtonTypeEnum.BUTTON_TYPE_RESUBMIT.getCode())
+                && !vo.getOperationType().equals(ButtonTypeEnum.BUTTON_TYPE_AGREE.getCode()) ){
+            BizUcarfuel ucarFuel = new BizUcarfuel();
+            BeanUtils.copyProperties(vo,ucarFuel);
+            Integer id=  Integer.valueOf((vo.getBusinessId()).toString());
+            ucarFuel.setId(id);
+            bizUcarfuelMapper.updateById(ucarFuel);
+        }
+    }
+
+    @Override
+    public void backToModifyData(BizUcarRefuelVo vo) {
+
+    }
+
+    @Override
+    public void cancellationData(BizUcarRefuelVo vo) {
+
+    }
+
+    @Override
+    public void finishData(BusinessDataVo vo) {
+
+    }
+}
