3ceb751829a28db3385f5b67b417932a9014039b
MorisWu <3081045655@qq.com>
2025-12-16 19:10:29 +0800

    flowoa-vue/src/views/workflow/flowMsg flowoa-vue/src/views/workflow/taskCenter

---
+++ flowoa-vue/src/views/workflow/flowMsg/msgTemplete/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="app-container">
+        <el-tabs :tab-position="tabPosition" v-model="activeName">
+            <el-tab-pane label="通用模板" name="comTemplate">
+                <com-template-list v-if="activeName == 'comTemplate'" />
+            </el-tab-pane>
+            <el-tab-pane label="短信模板" name="smsTemplate">
+                <sms-template-list v-if="activeName == 'smsTemplate'" />
+            </el-tab-pane>
+        </el-tabs>
+    </div>
+</template>
+
+<script setup>
+import comTemplateList from "./templates/comTemplateList.vue";
+import smsTemplateList from "./templates/smsTemplateList.vue";
+const tabPosition = ref('left')
+const activeName = ref('comTemplate')
+</script>
+<style scoped>
+::v-deep(.el-tabs__item.is-left.is-active) {
+    background-color: var(--current-color) !important;
+    color: var(--navbar-bg);
+}
+
+::v-deep(.el-tabs__header) {
+    min-height: 300px;
+}
+
+::v-deep(.el-tabs__header) {
+    background-color: var(--navbar-bg) !important;
+    font-size: 18px;
+}
+</style>
